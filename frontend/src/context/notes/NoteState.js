7094fa1e5a7f390750d7901f875c65019934bed8
Rohit Mittal <rohit1199.be21@chitkara.edu.in>
2024-05-16 00:30:06 +0530

    updated url in notestate to env port

--- frontend/src/context/notes/NoteState.js
+++ frontend/src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import NoteContext from "./NoteContext";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000";
+  const host = process.env.REACT_APP_B_URL;
   const [notes, setNotes] = useState([]);
 
   // Get All notes 
